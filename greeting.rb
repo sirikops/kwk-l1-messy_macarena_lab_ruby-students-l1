2774eebaedb76e8ef553db5b955e956505861a29
sirikops <sirikops@hotmail.com>
2018-06-04 22:47:42 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -5,3 +5,6 @@ def greeting
   puts "Where do you come from?"
 country_of_origin = gets
 puts "That's cool! I'm from the USA!"
+puts "Is it the morning, afternoon, or evening?"
+time_of_day = gets
+puts "Hope you have a nice #{time_of_day}!"
