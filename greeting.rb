59b0ea7ed2e137596228b7c76c24e5ecc47bfcd9
sirikops <sirikops@hotmail.com>
2018-06-04 22:44:48 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -1,7 +1,7 @@
 def greeting
   puts "What's your name?"
   your_name = gets
-  puts "Hi #{your_name} I'm Siri!"
-end
-
-greeting
+  puts "Hi #{your_name}! I'm Siri."
+  puts "Where do you come from?"
+country_of_origin = gets
+puts "That's cool! I'm from the USA!"
